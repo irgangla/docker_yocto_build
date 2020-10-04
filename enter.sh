a2a82762abdf933f85e1802bce08db7727e05fd1
Thomas Irgang <thomas@irgang.eu>
2020-10-04 09:00:08 +0200

    add home folder

--- enter.sh
+++ enter.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
 docker rm yocto_build
-docker run -h yocto --name yocto_build -v $(pwd):/yocto -it  $USER/yocto /bin/bash
+docker run -h yocto --name yocto_build -v $(pwd):/yocto -v $(pwd)/home:/home/yb -it  $USER/yocto /bin/bash
 
